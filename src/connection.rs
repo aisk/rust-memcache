9b4adc49b620a36ce065f4bb48c629d1d3646e5e
Paulo Suzart <paulosuzart@gmail.com>
2015-04-12 13:28:52 -0300

    Version with multi server support

--- src/connection.rs
+++ src/connection.rs
@@ -1,6 +1,8 @@
 use std::convert::From;
 use std::io::{BufStream, Error, Write, Read, BufRead};
 use std::net::TcpStream;
+use hash_ring::{NodeInfo, HashRing};
+use std::collections::HashMap;
 
 #[derive(Debug)]
 pub enum MemcacheError {
@@ -16,14 +18,103 @@ impl From<Error> for MemcacheError {
 
 pub type MemcacheResult<T> = Result<T, MemcacheError>;
 
+// Trait defining Memcache protocol both for multi server connection using `Client` as
+// well as single connection using `Connection`.
+pub trait Commands {
+    fn flush(&mut self) -> MemcacheResult<()>;
+    fn delete(&mut self, key: &str) -> MemcacheResult<bool>;
+    fn get(&mut self, key: &str) -> MemcacheResult<Option<(Vec<u8>, u16)>>;
+    fn set(&mut self, key: &str, value: &[u8], exptime: isize, flags: u16) -> MemcacheResult<bool>;
+    fn incr(&mut self, key: &str, value: u64) -> MemcacheResult<Option<(isize)>>;
+    fn decr(&mut self, key: &str, value: u64) -> MemcacheResult<Option<(isize)>>;
+}
+
+// Actual memcached client. Holds all connections and perform appropriate job using
+// Consistent Hash Ring to balanced between servers and virtual replicas.
+pub struct Client {
+    ring : HashRing<NodeInfo>,
+    connections : HashMap<String, Connection>
+}
+
+impl Client {
+    // `replicas` are copies of nodes that point to real servers.
+    fn new(nodes: Vec<NodeInfo>, replicas: isize) -> MemcacheResult<Client> {
+        let mut new_client = Client {
+           ring : HashRing::new(nodes.clone(), replicas),
+           connections : HashMap::with_capacity(nodes.len())
+        };
+        
+        // since BufStream can't be clonned. The connections reside in Client. So node lookups are then mapped
+        // and commands executed using the right connection regardles if a read or virtual node.
+        for n in nodes {
+            let conn = try!{Connection::connect(n.host, n.port)};
+            new_client.connections.insert(n.to_string(), conn);
+        }
+
+        Ok(new_client)
+    }
+}
+
+impl Commands for Client {
+    
+    fn set(&mut self, key: &str, value: &[u8], exptime: isize, flags: u16) -> MemcacheResult<bool> {
+        let node = self.ring.get_node(key.to_string());
+        let conn = self.connections.get_mut(&node.to_string()).expect("Inexistent Connection");
+        conn.set(key, value, exptime, flags)
+    }
+
+    fn get(&mut self, key: &str) -> MemcacheResult<Option<(Vec<u8>, u16)>> {
+        let node = self.ring.get_node(key.to_string());
+        let conn = self.connections.get_mut(&node.to_string()).expect("Inexistent Connection");
+        conn.get(key)
+    }
+    
+    fn flush(&mut self) -> MemcacheResult<()> {
+        for (_, conn) in self.connections.iter_mut() {
+            match conn.flush() {
+                Ok(_) => continue,
+                e => return e
+            }
+        }
+        Ok(())
+    }
+
+    fn delete(&mut self, key: &str) -> MemcacheResult<bool> {
+        let node = self.ring.get_node(key.to_string());
+        let conn = self.connections.get_mut(&node.to_string()).expect("Inexistent Connection");
+        conn.delete(key)
+    }
+
+    fn incr(&mut self, key: &str, value: u64) -> MemcacheResult<Option<(isize)>> {
+        let node = self.ring.get_node(key.to_string());
+        let conn = self.connections.get_mut(&node.to_string()).expect("Inexistent Connection");
+        conn.incr(key, value)
+    }
+
+    fn decr(&mut self, key: &str, value: u64) -> MemcacheResult<Option<(isize)>> {
+        let node = self.ring.get_node(key.to_string());
+        let conn = self.connections.get_mut(&node.to_string()).expect("Inexistent Connection");
+        conn.decr(key, value)        
+    }
+
+}
+
 pub struct Connection {
     pub host: String,
     pub port: u16,
     stream: BufStream<TcpStream>,
 }
 
-impl Connection {
-    pub fn flush(&mut self) -> MemcacheResult<()> {
+impl ToString for Connection {
+    fn to_string(&self) -> String {
+        format!("{}:{}", self.host, self.port)
+    }
+}
+
+
+
+impl Commands for Connection {
+    fn flush(&mut self) -> MemcacheResult<()> {
         try!{ self.stream.write("flush_all\r\n".as_bytes()) };
         try!{ self.stream.flush() };
         let mut line : String = String::new();
@@ -34,7 +125,7 @@ impl Connection {
         return Ok(());
     }
 
-    pub fn delete(&mut self, key: &str) -> MemcacheResult<bool> {
+    fn delete(&mut self, key: &str) -> MemcacheResult<bool> {
         try!{ self.stream.write(format!("delete {}\r\n", key).as_bytes()) };
         try!{ self.stream.flush() };
         let mut line : String = String::new();
@@ -48,7 +139,7 @@ impl Connection {
         }
     }
 
-    pub fn get(&mut self, key: &str) -> MemcacheResult<Option<(Vec<u8>, u16)>> {
+    fn get(&mut self, key: &str) -> MemcacheResult<Option<(Vec<u8>, u16)>> {
         try!{ self.stream.write(format!("get {}\r\n", key).as_bytes()) };
         try!{ self.stream.flush() };
         let mut line : String = String::new();
@@ -77,7 +168,7 @@ impl Connection {
         return Ok(Some((buf, flags)));
     }
 
-    pub fn set(&mut self, key: &str, value: &[u8], exptime: isize, flags: u16) -> MemcacheResult<bool> {
+    fn set(&mut self, key: &str, value: &[u8], exptime: isize, flags: u16) -> MemcacheResult<bool> {
         try!{ self.stream.write(format!("set {} {} {} {}\r\n", key, flags, exptime, value.len()).as_bytes()) };
         try!{ self.stream.write(value) };
         try!{ self.stream.write("\r\n".as_bytes()) };
@@ -92,7 +183,7 @@ impl Connection {
         return Err(MemcacheError::ServerError);
     }
 
-    pub fn incr(&mut self, key: &str, value: u64) -> MemcacheResult<Option<(isize)>> {
+    fn incr(&mut self, key: &str, value: u64) -> MemcacheResult<Option<(isize)>> {
         try!{ self.stream.write(format!("incr {} {:b}\r\n", key, value).as_bytes()) };
         try!{ self.stream.flush() };
         let mut line : String = String::new();
@@ -109,7 +200,7 @@ impl Connection {
         return Ok(Some(value));
     }
 
-    pub fn decr(&mut self, key: &str, value: u64) -> MemcacheResult<Option<(isize)>> {
+    fn decr(&mut self, key: &str, value: u64) -> MemcacheResult<Option<(isize)>> {
         try!{ self.stream.write(format!("decr {} {:b}\r\n", key, value).as_bytes()) };
         try!{ self.stream.flush() };
         let mut line : String = String::new();
@@ -125,16 +216,120 @@ impl Connection {
         };
         return Ok(Some(value));
     }
+}
 
+impl Connection {
     pub fn connect(host: &str, port: u16) -> MemcacheResult<Connection> {
-        let stream = try!{ TcpStream::connect((host, port)) };
-        return Ok(Connection{
+    let stream = try!{ TcpStream::connect((host, port)) };
+    return Ok(Connection{
             host: host.to_string(),
             port: port,
             stream: BufStream::new(stream)
         });
     }
 }
+
+
+#[test]
+fn test_client() {
+    let mut nodes: Vec<NodeInfo> = Vec::new();
+    nodes.push(NodeInfo{host: "localhost", port: 2333});
+    nodes.push(NodeInfo{host: "localhost", port: 2334});
+
+    let client = Client::new(nodes, 2);
+    assert! { client.is_ok() };
+}
+
+
+#[test]
+fn test_client_flush() {
+    let mut nodes: Vec<NodeInfo> = Vec::new();
+    nodes.push(NodeInfo{host: "localhost", port: 2333});
+    nodes.push(NodeInfo{host: "localhost", port: 2334});
+
+    let mut client = Client::new(nodes, 2).ok().unwrap();
+    assert!{ client.flush().is_ok() };
+}
+
+#[test]
+fn test_client_set() {
+    let mut nodes: Vec<NodeInfo> = Vec::new();
+    nodes.push(NodeInfo{host: "localhost", port: 2333});
+    nodes.push(NodeInfo{host: "localhost", port: 2334});
+
+    let mut client = Client::new(nodes, 2).ok().unwrap();
+    assert!{ client.flush().is_ok() };
+    assert!{ client.set("foo", b"bar", 10, 0).ok().unwrap() == true };
+}
+
+#[test]
+fn test_client_get() {
+    let mut nodes: Vec<NodeInfo> = Vec::new();
+    nodes.push(NodeInfo{host: "localhost", port: 2333});
+    nodes.push(NodeInfo{host: "localhost", port: 2334});
+
+    let mut client = Client::new(nodes, 2).ok().unwrap();
+
+    assert!{ client.flush().is_ok() };
+    assert!{ client.get("foo").ok().unwrap() == None };
+
+    assert!{ client.set("foo", b"bar", 0, 10).ok().unwrap() == true };
+    let result = client.get("foo");
+    let result_tuple = result.ok().unwrap().unwrap();
+    assert!{ result_tuple.0 == b"bar" };
+    assert!{ result_tuple.1 == 10 };
+}
+
+
+#[test]
+fn test_client_delete() {
+    let mut nodes: Vec<NodeInfo> = Vec::new();
+    nodes.push(NodeInfo{host: "localhost", port: 2333});
+    nodes.push(NodeInfo{host: "localhost", port: 2334});
+
+    let mut client = Client::new(nodes, 2).ok().unwrap();
+
+    assert!{ client.flush().is_ok() };
+    assert!{ client.delete("foo").ok().unwrap() == false };
+}
+
+#[test]
+fn test_client_incr() {
+    let mut nodes: Vec<NodeInfo> = Vec::new();
+    nodes.push(NodeInfo{host: "localhost", port: 2333});
+    nodes.push(NodeInfo{host: "localhost", port: 2334});
+
+    let mut client = Client::new(nodes, 2).ok().unwrap();
+    assert!{ client.flush().is_ok() };
+    let mut result = client.incr("lie", 42);
+    assert!{ result.ok().unwrap() == None };
+
+    assert!{ client.flush().is_ok() };
+    client.set("truth", b"42", 0, 0).ok().unwrap();
+    result = client.incr("truth", 1);
+    assert!{ result.ok().unwrap().unwrap() == 43 };
+}
+
+
+#[test]
+fn test_client_decr() {
+    let mut nodes: Vec<NodeInfo> = Vec::new();
+    nodes.push(NodeInfo{host: "localhost", port: 2333});
+    nodes.push(NodeInfo{host: "localhost", port: 2334});
+    let mut client = Client::new(nodes, 2).ok().unwrap();
+    assert!{ client.flush().is_ok() };
+
+    let mut result = client.decr("lie", 42);
+    assert!{ result.ok().unwrap() == None };
+
+    assert!{ client.flush().is_ok() };
+    client.set("truth", b"42", 0, 0).ok().unwrap();
+    result = client.decr("truth", 1);
+    assert!{ result.ok().unwrap().unwrap() == 41 };
+}
+
+
+// Testing single connected servers
 
 #[test]
 fn test_connect() {
