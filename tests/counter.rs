2c4bf10cd40fa64794364d449e231834f4f0c690
Ān Lóng <aisk1988@gmail.com>
2015-10-26 21:20:50 +0800

    add counter test

--- tests/counter.rs
+++ tests/counter.rs
@@ -5,6 +5,9 @@ fn test_counter() {
     let client = memcache::connect(&("localhost", 2333)).unwrap();
     client.flush(0).unwrap();
 
-    client.set_raw("truth", &[0x0u8], 0, 0).unwrap();
+    client.set_raw("truth", &[0x30u8], 0, 0).unwrap();
     client.increment("truth", 42).unwrap();
+    let (value, flag) = client.get_raw("truth").unwrap();
+    assert!(flag == 0);
+    assert!(value == &[0x34, 0x32]);
 }
