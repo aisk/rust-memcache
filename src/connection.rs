de57f467ba40b0eec4e13597cd491bfd85662607
Long Ang <aisk1988@gmail.com>
2016-10-24 23:27:53 +0800

    feat: use BufReader instead of TcpStream

--- src/connection.rs
+++ src/connection.rs
@@ -1,24 +1,29 @@
-use std::io;
 use std::net;
+use std::io;
 use std::io::Write;
+use std::io::BufRead;
 
 pub struct Connection {
-    stream: net::TcpStream
+    reader: io::BufReader<net::TcpStream>
 }
 
 impl Connection {
     pub fn flush(&mut self) -> io::Result<()> {
-        match self.stream.write(b"flush_all\r\n") {
+        match self.reader.get_ref().write(b"flush_all\r\n") {
             Ok(_) => {},
             Err(err) => return Err(err),
         }
-        return self.stream.flush();
+        try!(self.reader.get_ref().flush());
+        let mut s = String::new();
+        let _ = self.reader.read_line(&mut s);
+        s == "OK\r\n";  // TODO: assert it
+        return Ok(());
     }
 }
 
 pub fn connect()-> Result<Connection, io::Error>{
     match net::TcpStream::connect("127.0.0.1:11211") {
-        Ok(stream) => return Ok(Connection{stream: stream}),
+        Ok(stream) => return Ok(Connection{reader: io::BufReader::new(stream)}),
         Err(err) => return Err(err),
     }
 }
