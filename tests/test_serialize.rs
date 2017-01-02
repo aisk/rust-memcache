9d740ae82a8ee71c36d985e7edd688f250d47833
Long Ang <aisk1988@gmail.com>
2017-01-02 23:19:50 +0800

    test: more tests

---
+++ tests/test_serialize.rs
@@ -0,0 +1,14 @@
+extern crate memcache;
+
+#[test]
+fn set_string() {
+    let mut conn = memcache::connection::connect("127.0.0.1:12345").unwrap();
+    conn.set("this_is_a_string", String::from("a string"), 0).unwrap();
+    conn.set("this_is_another_string", "another string", 0).unwrap();
+}
+
+#[test]
+fn set_bytes() {
+    let mut conn = memcache::connection::connect("127.0.0.1:12345").unwrap();
+    conn.set("this_is_a_bytes", "some bytes".as_bytes(), 0).unwrap();
+}
