c49d7c33626c82ad39680f938ac834975640596d
Long Ang <aisk1988@gmail.com>
2017-01-12 23:56:24 +0800

    feat: support store command with exptime

---
+++ src/options.rs
@@ -0,0 +1,6 @@
+#[derive(Debug, Default)]
+pub struct Options {
+    pub no_reply: bool,
+    pub exptime: u32,
+    pub flags: u32,
+}
---
+++ tests/test_options.rs
@@ -0,0 +1,18 @@
+extern crate memcache;
+
+#[test]
+fn test_default() {
+    let options: &memcache::Options = &Default::default();
+    assert!(options.exptime == 0);
+    assert!(options.flags == 0);
+    assert!(options.no_reply == false);
+}
+
+#[test]
+fn test_exptime() {
+    let mut conn = memcache::Connection::connect("localhost:12345").unwrap();
+    conn.set_with_options("key_with_exptime", "plus one second",&memcache::Options{
+        exptime: 1234,
+        ..Default::default()
+    }).unwrap();
+}
