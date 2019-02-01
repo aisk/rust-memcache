70d89ef87c7c703d87f2c3ceec2201fb2f665a48
An Long <aisk1988@gmail.com>
2019-02-02 00:25:26 +0800

    :construction: add assci protocol to protocol enum

--- src/protocol/mod.rs
+++ src/protocol/mod.rs
@@ -7,100 +7,117 @@ use client::Stats;
 use stream::Stream;
 use error::MemcacheError;
 use value::{ToMemcacheValue, FromMemcacheValue};
+pub(crate) use protocol::ascii::AsciiProtocol;
 pub(crate) use protocol::binary::BinaryProtocol;
 
 pub enum Protocol {
+    Ascii(AsciiProtocol),
     Binary(BinaryProtocol),
 }
 
 impl Protocol {
     pub(super) fn version(&mut self) -> Result<String, MemcacheError> {
         match self {
-            Protocol::Binary(ref mut protocol) => protocol.version()
+            Protocol::Ascii(ref mut protocol) => unimplemented!(),
+            Protocol::Binary(ref mut protocol) => protocol.version(),
         }
     }
 
     pub(super) fn flush(&mut self) -> Result<(), MemcacheError> {
         match self {
-            Protocol::Binary(ref mut protocol) => protocol.flush()
+            Protocol::Ascii(ref mut protocol) => unimplemented!(),
+            Protocol::Binary(ref mut protocol) => protocol.flush(),
         }
     }
 
     pub(super) fn flush_with_delay(&mut self, delay: u32) -> Result<(), MemcacheError> {
         match self {
-            Protocol::Binary(ref mut protocol) => protocol.flush_with_delay(delay)
+            Protocol::Ascii(ref mut protocol) => unimplemented!(),
+            Protocol::Binary(ref mut protocol) => protocol.flush_with_delay(delay),
         }
     }
 
     pub(super) fn get<V: FromMemcacheValue>(&mut self, key: &str) -> Result<Option<V>, MemcacheError> {
         match self {
-            Protocol::Binary(ref mut protocol) => protocol.get(key)
+            Protocol::Ascii(ref mut protocol) => unimplemented!(),
+            Protocol::Binary(ref mut protocol) => protocol.get(key),
         }
     }
 
     pub(super) fn gets<V: FromMemcacheValue>(&mut self, keys: Vec<&str>) -> Result<HashMap<String, V>, MemcacheError> {
         match self {
-            Protocol::Binary(ref mut protocol) => protocol.gets(keys)
+            Protocol::Ascii(ref mut protocol) => unimplemented!(),
+            Protocol::Binary(ref mut protocol) => protocol.gets(keys),
         }
     }
 
     pub(super) fn set<V: ToMemcacheValue<Stream>>(&mut self, key: &str, value: V, expiration: u32) -> Result<(), MemcacheError> {
         match self {
-            Protocol::Binary(ref mut protocol) => protocol.set(key, value, expiration)
+            Protocol::Ascii(ref mut protocol) => unimplemented!(),
+            Protocol::Binary(ref mut protocol) => protocol.set(key, value, expiration),
         }
     }
 
     pub(super) fn add<V: ToMemcacheValue<Stream>>(&mut self, key: &str, value: V, expiration: u32) -> Result<(), MemcacheError> {
         match self {
-            Protocol::Binary(ref mut protocol) => protocol.add(key, value, expiration)
+            Protocol::Ascii(ref mut protocol) => unimplemented!(),
+            Protocol::Binary(ref mut protocol) => protocol.add(key, value, expiration),
         }
     }
 
     pub(super) fn replace<V: ToMemcacheValue<Stream>>(&mut self, key: &str, value: V, expiration: u32) -> Result<(), MemcacheError> {
         match self {
-            Protocol::Binary(ref mut protocol) => protocol.replace(key, value, expiration)
+            Protocol::Ascii(ref mut protocol) => unimplemented!(),
+            Protocol::Binary(ref mut protocol) => protocol.replace(key, value, expiration),
         }
     }
 
     pub(super) fn append<V: ToMemcacheValue<Stream>>(&mut self, key: &str, value: V) -> Result<(), MemcacheError> {
         match self {
-            Protocol::Binary(ref mut protocol) => protocol.append(key, value)
+            Protocol::Ascii(ref mut protocol) => unimplemented!(),
+            Protocol::Binary(ref mut protocol) => protocol.append(key, value),
         }
     }
 
     pub(super) fn prepend<V: ToMemcacheValue<Stream>>(&mut self, key: &str,value: V) -> Result<(), MemcacheError> {
         match self {
-            Protocol::Binary(ref mut protocol) => protocol.prepend(key, value)
+            Protocol::Ascii(ref mut protocol) => unimplemented!(),
+            Protocol::Binary(ref mut protocol) => protocol.prepend(key, value),
         }
     }
 
     pub(super) fn delete(&mut self, key: &str) -> Result<bool, MemcacheError> {
         match self {
-            Protocol::Binary(ref mut protocol) => protocol.delete(key)
+            Protocol::Ascii(ref mut protocol) => unimplemented!(),
+            Protocol::Binary(ref mut protocol) => protocol.delete(key),
         }
     }
 
     pub(super) fn increment(&mut self, key: &str, amount: u64) -> Result<u64, MemcacheError> {
         match self {
-            Protocol::Binary(ref mut protocol) => protocol.increment(key, amount)
+            Protocol::Ascii(ref mut protocol) => unimplemented!(),
+            Protocol::Binary(ref mut protocol) => protocol.increment(key, amount),
         }
     }
 
     pub(super) fn decrement(&mut self, key: &str, amount: u64) -> Result<u64, MemcacheError> {
         match self {
-            Protocol::Binary(ref mut protocol) => protocol.decrement(key, amount)
+            Protocol::Ascii(ref mut protocol) => unimplemented!(),
+            Protocol::Binary(ref mut protocol) => protocol.decrement(key, amount),
         }
     }
 
     pub(super) fn touch(&mut self, key: &str, expiration: u32) -> Result<bool, MemcacheError> {
         match self {
-            Protocol::Binary(ref mut protocol) => protocol.touch(key, expiration)
+            Protocol::Ascii(ref mut protocol) => unimplemented!(),
+            Protocol::Binary(ref mut protocol) => protocol.touch(key, expiration),
         }
     }
 
     pub(super) fn stats(&mut self) -> Result<Stats, MemcacheError> {
         match self {
-            Protocol::Binary(ref mut protocol) => protocol.stats()
+            Protocol::Ascii(ref mut protocol) => unimplemented!(),
+            Protocol::Binary(ref mut protocol) => protocol.stats(),
         }
     }
 }
