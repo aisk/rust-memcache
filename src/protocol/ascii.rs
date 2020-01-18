d16f89e2bb2bc72ae97b59491cb3c2f69fadad17
Rohit Kumar <letmutx@gmail.com>
2020-01-18 21:17:22 +0530

    implement our own buf reader

--- src/protocol/ascii.rs
+++ src/protocol/ascii.rs
@@ -1,6 +1,6 @@
 use std::collections::HashMap;
 use std::fmt;
-use std::io::{BufRead, BufReader, Read, Write};
+use std::io::{Read, Write};
 
 use super::check_key_len;
 use client::Stats;
@@ -41,11 +41,84 @@ impl fmt::Display for StoreCommand {
         }
     }
 }
+
+struct MyBufReader<C> {
+    inner: C,
+    filled: usize,
+    buf: Box<[u8]>,
+}
+
+fn get_line(buf: &[u8]) -> Option<usize> {
+    for (i, r) in buf.iter().enumerate() {
+        if *r == b'\r' {
+            if buf.get(i + 1) == Some(&b'\n') {
+                return Some(i + 2);
+            }
+        }
+    }
+    None
+}
+
+impl<C: Read> MyBufReader<C> {
+    fn with_capacity(capacity: usize, inner: C) -> Self {
+        let mut buf = Vec::with_capacity(capacity);
+        unsafe { buf.set_len(capacity) };
+        Self {
+            inner,
+            filled: 0,
+            buf: buf.into_boxed_slice(),
+        }
+    }
+
+    fn get_mut(&mut self) -> &mut C {
+        &mut self.inner
+    }
+
+    fn read_exact(&mut self, buf: &mut [u8]) -> std::io::Result<()> {
+        buf.copy_from_slice(&self.buf[..self.filled]);
+        self.filled = 0;
+        self.inner.read_exact(&mut buf[self.filled..])
+    }
+
+    fn read_line(&mut self) -> std::io::Result<&[u8]> {
+        // check if the buf already has a new line
+        if let Some(n) = get_line(&self.buf[..self.filled]) {
+            return Ok(&self.buf[..n]);
+        }
+        loop {
+            let (filled, buf) = self.buf.split_at_mut(self.filled);
+            let filled = filled.len();
+            let read = self.inner.read(&mut buf[..])?;
+            if read == 0 {
+                // TODO: change to error
+                panic!("{:?} {:?} line too long", filled, buf.len());
+            }
+            self.filled += read;
+            if let Some(n) = get_line(&buf[..read]) {
+                return Ok(&self.buf[..filled + n]);
+            }
+        }
+    }
+
+    fn consume(&mut self, amount: usize) {
+        let amount = std::cmp::min(self.filled, amount);
+        self.buf.copy_within(amount..self.filled, 0);
+        self.filled -= amount;
+    }
+}
+
 pub struct AsciiProtocol<C: Read + Write + Sized> {
-    pub reader: BufReader<C>,
+    pub reader: MyBufReader<C>,
 }
 
 impl AsciiProtocol<Stream> {
+
+    pub(crate) fn new(stream: Stream) -> Self {
+        Self {
+            reader: MyBufReader::with_capacity(2*1024, stream)
+        }
+    }
+
     pub(super) fn auth(&mut self, username: &str, password: &str) -> Result<(), MemcacheError> {
         return self.set("auth", format!("{} {}", username, password), 0);
     }
