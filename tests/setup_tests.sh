74ce330e3dae05d37d6add45e9440d935a643479
An Long <aisk1988@gmail.com>
2020-02-01 13:46:23 +0800

    :see_no_evil: update .gitignore to add memcached folder

--- tests/setup_tests.sh
+++ tests/setup_tests.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 set -e
 
+BASEDIR=$(dirname "$0")
 MEMCACHED_VERSION="1.5.20"
 MEMCACHED_TARBALL="memcached-$MEMCACHED_VERSION.tar.gz"
-MEMCACHED_DIR="memcached-$MEMCACHED_VERSION"
+MEMCACHED_DIR="$BASEDIR/memcached-$MEMCACHED_VERSION"
 MEMCACHED="$MEMCACHED_DIR/memcached"
 
-SSL_KEY=tests/assets/localhost.key
-SSL_CERT=tests/assets/localhost.crt
-SSL_ROOT_CERT=tests/assets/RUST_MEMCACHE_TEST_CERT.crt
+SSL_KEY=$BASEDIR/assets/localhost.key
+SSL_CERT=$BASEDIR/assets/localhost.crt
+SSL_ROOT_CERT=$BASEDIR/assets/RUST_MEMCACHE_TEST_CERT.crt
 
 echo "Building memcached $MEMCACHED_VERSION with TLS support"
 if [[ ! -d "$MEMCACHED_DIR" ]]; then
     curl "https://memcached.org/files/$MEMCACHED_TARBALL" -O
-    tar xvf "$MEMCACHED_TARBALL"
+    tar xvf "$MEMCACHED_TARBALL" -C "$BASEDIR"
+    rm "$MEMCACHED_TARBALL"
 fi
 
 if [[ ! -f "$MEMCACHED" ]]; then
